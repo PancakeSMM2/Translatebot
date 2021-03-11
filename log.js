768c07f2193b4875df69b6bff1c26dce224d5f6d
PancakeSMM2 <smm2pancake@gmail.com>
2021-03-11 11:09:01 -0600

    hopefully(?) fixed it

--- log.js
+++ log.js
@@ -4,14 +4,17 @@ const client = require('./client')
 const config = require('./config.json')
 
 module.exports = (text) => {
-  const logServer = client.guilds.resolve(config.logServerID)
+  let logServer = client.guilds.resolve(config.logServerID)
 
-  if (!logServer) return
-  if (!logServer.available) return
-  const logChannel = logServer.channels.resolve(config.logChannelID)
+  if (logServer) {
+    logServer = logServer.available ? logServer : null
+  }
+  const logChannel = logServer ? logServer.channels.resolve(config.logChannelID) : null
 
-  if (logChannel.isText()) {
-    logChannel.send(text)
+  if (logChannel !== null) {
+    if (logChannel.isText()) {
+      logChannel.send(text)
+    }
   }
   console.log(text)
 }
